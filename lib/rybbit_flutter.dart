ac9f8c7604bc7109908cc87da652b108ab7f3f7f
LuRy <lury@lury.cz>
2026-02-24 14:02:56 +0100

    feat: add NavigatorObserver for auto screen tracking

--- lib/rybbit_flutter.dart
+++ lib/rybbit_flutter.dart
@@ -4,4 +4,5 @@ export 'src/config.dart';
 export 'src/models/event_type.dart';
 export 'src/models/track_payload.dart';
 export 'src/models/identify_payload.dart';
+export 'src/navigator_observer.dart';
 export 'src/rybbit.dart';
---
+++ lib/src/navigator_observer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+import 'rybbit.dart';
+
+class RybbitNavigatorObserver extends NavigatorObserver {
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    _trackRoute(route);
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    if (previousRoute != null) {
+      _trackRoute(previousRoute);
+    }
+  }
+
+  @override
+  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
+    if (newRoute != null) {
+      _trackRoute(newRoute);
+    }
+  }
+
+  void _trackRoute(Route<dynamic> route) {
+    final name = route.settings.name;
+    if (name != null && name.isNotEmpty && Rybbit.isInitialized) {
+      Rybbit.instance.screenView(
+        name,
+        title: _extractTitle(route),
+      );
+    }
+  }
+
+  String? _extractTitle(Route<dynamic> route) {
+    final args = route.settings.arguments;
+    if (args is Map<String, dynamic> && args.containsKey('title')) {
+      return args['title'] as String?;
+    }
+    return route.settings.name;
+  }
+}
